8c4e32b56d38d64d130325288b92e9dde7f396a3
302kt <kpf.freire@gmail.com>
2023-06-07 16:21:18 -0300

    resolvendo conflito

--- src/componentes/estaticos/navbar/Navbar.tsx
+++ src/componentes/estaticos/navbar/Navbar.tsx
@@ -55,10 +55,10 @@ navbarComponent = (
  
       <AppBar position="static" style={{ backgroundColor: "#996fcf" }}>
         <Toolbar variant="dense">
-          
+
           <Link to="/home" className="text-decorator-none">
             <img
-              src="src/assets/images/logo_ufa_sem_fundo.png"
+              src="src/assets/images/ufa_logo_roxo.png"
               alt="Logo ufa"
               width="100px"
               height="100px"
@@ -67,7 +67,7 @@ navbarComponent = (
           <Box marginLeft="auto">
             <IconButton
               edge="end"
-              style={{ color: "#ff0054" }}
+              style={{ color: "black" }}
               aria-label="menu"
               onClick={toggleDrawer(true)}
             >
@@ -78,17 +78,17 @@ navbarComponent = (
               open={isDrawerOpen}
               onClose={toggleDrawer(false)}
             >
-              <List style={{ backgroundColor: "#ffb3c6" }}>
+              <List style={{ backgroundColor: "black" }}>
                 <Link to="/home" className="text-decorator-none">
                   <ListItem button>
-                    <ListItemText primary="Home" style={{ color: "#ff0054" }} />
+                    <ListItemText primary="Home" style={{ color: "white" }} />
                   </ListItem>
                 </Link>
                 <Link to="/formularioPostagem" className="text-decorator-none">
                   <ListItem button>
                     <ListItemText
                       primary="Criar Postagem"
-                      style={{ color: "#ff0054" }}
+                      style={{ color: "white" }}
                     />
                   </ListItem>
                 </Link>
@@ -96,7 +96,7 @@ navbarComponent = (
                   <ListItem button>
                     <ListItemText
                       primary="Sobre"
-                      style={{ color: "#ff0054" }}
+                      style={{ color: "#white" }}
                     />
                   </ListItem>
                 </Link>
@@ -104,7 +104,7 @@ navbarComponent = (
                   <ListItem button>
                     <ListItemText
                       primary="Postagens"
-                      style={{ color: "#ff0054" }}
+                      style={{ color: "white" }}
                     />
                   </ListItem>
                 </Link>
@@ -112,7 +112,7 @@ navbarComponent = (
                   <ListItem button>
                     <ListItemText
                       primary="Temas"
-                      style={{ color: "#ff0054" }}
+                      style={{ color: "white" }}
                     />
                   </ListItem>
                 </Link>
@@ -120,7 +120,7 @@ navbarComponent = (
                   <ListItem button>
                     <ListItemText
                       primary="Cadastrar Temas"
-                      style={{ color: "#ff0054" }}
+                      style={{ color: "white" }}
                     />
                   </ListItem>
                 </Link>
@@ -128,7 +128,7 @@ navbarComponent = (
                   <ListItem button>
                     <ListItemText
                       primary="Contatos"
-                      style={{ color: "#ff0054" }}
+                      style={{ color: "white" }}
                     />
                   </ListItem>
                 </Link>
@@ -136,13 +136,13 @@ navbarComponent = (
                   <ListItem button>
                     <ListItemText
                       primary="Perfil"
-                      style={{ color: "#ff0054" }}
+                      style={{ color: "white" }}
                     />
                   </ListItem>
                 </Link>
                 {/* <Link to="/login" className="text-decorator-none"> */}
                 <ListItem button onClick={goLogout}>
-                  <ListItemText primary="Logout" style={{ color: "#ff0054" }} />
+                  <ListItemText primary="Logout" style={{ color: "white" }} />
                 </ListItem>
                 {/* </Link> */}
               </List>
