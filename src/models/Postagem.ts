36cab7691629da6c53d9c6fae2d798862080af97
Michelle Greghi <michelle.neves23@gmail.com>
2023-06-12 13:56:45 -0300

    Alterações na model Postagem

--- src/models/Postagem.ts
+++ src/models/Postagem.ts
@@ -2,16 +2,12 @@ import Tema from './Tema'
 import User from './User';
 
 interface Postagem{
-    comentarios: any;
-    likes: any;
     id:number;
     titulo:string;
     texto:string;
     data: string;
-    link:string;
     tema?:Tema| null
     usuario?: User| null
-
 }
 
 export default Postagem;
